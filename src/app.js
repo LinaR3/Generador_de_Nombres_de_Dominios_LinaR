76f48e77e41449c8af0bafd08de851baf404bd97
Lina Reina <linareinap3@gmail.com>
2025-10-07 20:39:11 +0000

    Generador de nombres de dominio HTML para el JS

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let tld = ['.com', '.net', '.us', '.io']; // El reto de las extensiones
+
+  let generatedDomains = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < tld.length; l++) {
+          let domainName = pronoun[i] + adj[j] + noun[k] + tld[l];
+          generatedDomains.push(domainName);
+        }
+      }
+    }
+  }
+
+
+  const resultsContainer = document.getElementById('results-list');
+  const domainCount = document.getElementById('domain-count');
+  if (resultsContainer && domainCount) {
+    let listHTML = '<ul>';
+    generatedDomains.forEach(domain => {
+      listHTML += `<li class="domain-item">${domain}</li>`;
+    });
+    listHTML += '</ul>';
+    
+    resultsContainer.innerHTML = listHTML;
+    
+    domainCount.textContent = generatedDomains.length;
+  }
 };
